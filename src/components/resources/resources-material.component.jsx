440c9a5d48aa7f94baef71370911cf497ff5ea46
Hitesh <hiteshdabur12@gmail.com>
2024-01-26 17:09:10 +0530

    I have added a resources page to the website.

---
+++ src/components/resources/resources-material.component.jsx
@@ -0,0 +1,11 @@
+const ResourcesMaterial = () => {
+    return (
+        <div class="flex items-center justify-center h-screen">
+            <div class="flex space-x-4 m-4" >
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/QCvL-DWcojc?si=5pY2LxRTByRmfjht" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe width="315" heignt="200" src="https://themes.getbootstrap.com/"></iframe>
+            </div>
+        </div>
+    )
+}
+export default ResourcesMaterial;
---
+++ src/routes/resources/resources.route.jsx
@@ -0,0 +1,13 @@
+import Resourcetop from "../../components/resources/resources.component";
+import ResourcesMaterial from "../../components/resources/resources-material.component";
+
+const ResourcesRoute = () => {
+    return (
+        <div>
+            <Resourcetop />
+            <ResourcesMaterial/>
+        </div>
+    )
+}
+
+export default ResourcesRoute;
